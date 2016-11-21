81a687bccd5c55fcf4eeb89cfc23bb290af47847
Mustafa Çelik <celikmus@gmail.com>
2016-11-21 22:58:23 +0000

    redux-thunk added, upgraded yarn packages.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,13 @@
 import {createStore, applyMiddleware, combineReducers, compose} from 'redux';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({root: () => ''});
 const env = process.env.NODE_ENV;
 export default function configureStore(initialState = {}) {
-  let enhancer = applyMiddleware({});
+  let enhancer = applyMiddleware(thunk);
   if (env === 'development') {
     const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f;
-    enhancer = compose(applyMiddleware({}), devTools);
+    enhancer = compose(applyMiddleware(thunk), devTools);
   }
   return createStore(
     rootReducer,
