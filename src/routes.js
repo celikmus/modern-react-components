c4a4a7d02a8369a6f15016d5aa9c6cfce2539f5e
Mustafa Çelik <celikmus@gmail.com>
2016-11-23 23:44:20 +0000

    Added Button to routes.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Route} from 'react-router';
 import App from './components/App';
+import Button from './components/Button/Button';
 
 export default (
   <Route path="/" component={App}>
+    <Route path="button" component={Button} key="1" />
   </Route>
 );
