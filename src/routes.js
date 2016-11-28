ec3659ae7a6c5b4bf7091bbff7c2f723a834d9c5
Mustafa Çelik <celikmus@gmail.com>
2016-11-28 22:23:00 +0000

    Added FilteringSelect to routes.

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ import {Route} from 'react-router';
 import App from './components/App';
 import Button from './components/Button/Button';
 import FilteringSelect from './components/FilteringSelect/FilteringSelect';
+import filteringSelectOptions from './store/filteringSelectOptions';
 
 export default (
   <Route path="/" component={App}>
     <Route path="button" component={Button} key="1" />
-    <Route path="filteringSelect" component={FilteringSelect} key="2" />
+    <Route path="filteringSelect" component={() => (<FilteringSelect options={filteringSelectOptions} />)} key="2" />
   </Route>
 );
