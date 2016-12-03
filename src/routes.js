5343d81b5770c57e133c6d0595893464231fd1b7
Mustafa Çelik <celikmus@gmail.com>
2016-12-03 21:29:39 +0000

    FilteringSelect: added name required parameter in instantiation.

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,6 @@ import filteringSelectOptions from './store/filteringSelectOptions';
 export default (
   <Route path="/" component={App}>
     <Route path="button" component={Button} key="1" />
-    <Route path="filteringSelect" component={() => (<FilteringSelect options={filteringSelectOptions} />)} key="2" />
+    <Route path="filteringSelect" component={() => (<FilteringSelect name="test" options={filteringSelectOptions} />)} key="2" />
   </Route>
 );
