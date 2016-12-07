adf54396726036ff7e106f6a5119762c1de91253
Mustafa Çelik <celikmus@gmail.com>
2016-12-07 16:23:44 +0000

    no message

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const serverConfig = {
   'development': {
     api: 'http://localhost:3001',
     apiPort: 3001,
-    devServerPort: 3000
+    devServerPort: 4000
   },
   'production': {
     api: 'https://myLovelyApiServer/api'
