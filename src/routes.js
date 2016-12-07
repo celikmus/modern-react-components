a55e39064893ebef24b145d3ebfc42366fd0c8bd
Mustafa Çelik <celikmus@gmail.com>
2016-12-07 22:56:41 +0000

    DateInput: added in routes.

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import App from './components/App';
 import Button from './components/Button/Button';
 import FilteringSelect from './components/FilteringSelect/FilteringSelect';
 import filteringSelectOptions from './store/filteringSelectOptions';
+import DateInput from './components/DateInput/DateInput';
 
 export default (
   <Route path="/" component={App}>
@@ -14,6 +15,8 @@ export default (
           name="test"
           options={filteringSelectOptions}
           changeHandler={(name, val) => { console.log(`${name} control value: ${val}`); }} />
-        )}key="2" />
+        )}
+      key="2" />
+    <Route path="dateInput" component={() => (<DateInput name="testDate" />)} key="3" />
   </Route>
 );
