ec1eecd59d6902229d8c57ba8625aa3a2c2a135f
Mustafa Çelik <celikmus@gmail.com>
2017-01-01 23:19:21 +0000

    Stored tab state locally.

--- src/components/TabContainer/TabContainer.js
+++ src/components/TabContainer/TabContainer.js
@@ -1,25 +1,54 @@
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 import './TabContainer.scss';
 
-const TabContainer = ({children, changeHandler, activeTab}) => {
-  const activeTabName = activeTab || children[0].props.name;
+class TabContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.handleOnClickTab = this.handleOnClickTab.bind(this);
+    this.state = {
+      activeTabName: props.activeTab || props.children[0].name
+    };
+  }
 
-  const renderHeader = () => children.map((child, i) => child.props.name);
-  const renderContent = () => {
+  handleOnClickTab(e) {
+    e.stopPropagation();
+    const activeTabName = e.target.name;
+    this.setState({activeTabName});
+    this.props.changeHandler(activeTabName);
+  }
+
+  renderHeader(activeTabName, children = []) {
+    return (
+      children.map((child, i) => <button
+        className={`tab-container__tab${(activeTabName === child.props.name) ? ' active' : ''}`}
+        name={child.props.name}
+        key={i}
+        onClick={this.handleOnClickTab}>
+        {child.props.name}
+      </button>
+    ));
+  }
+
+  renderContent(activeTabName, children = []) {
     const content = children.filter(child => child.props.name === activeTabName)[0];
     return content;
-  };
-  return (
-    <div className="tab-container">
-      <div className="tab-container__header">
-        {renderHeader()}
+  }
+
+  render() {
+    const {activeTabName} = this.state;
+    const {children} = this.props;
+    return (
+      <div className="tab-container">
+        <div className="tab-container__header">
+          {this.renderHeader(activeTabName, children)}
+        </div>
+        <div className="tab-container__content">
+          {this.renderContent(activeTabName, children)}
+        </div>
       </div>
-      <div className="tab-container__content">
-        {renderContent()}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 TabContainer.propTypes = {
   children: PropTypes.node.isRequired,
