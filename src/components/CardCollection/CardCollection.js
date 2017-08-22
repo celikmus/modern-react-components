72a545a84bba601ef5ca9bdd2987bd9f65824598
Mustafa Çelik <celikmus@gmail.com>
2017-08-22 18:06:52 +0100

    CardCollection layout.

--- src/components/CardCollection/CardCollection.js
+++ src/components/CardCollection/CardCollection.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './CardCollection.scss';
 
-const CardCollection = ({ children }) =>
-  <div className="card-collection">
-    {children}
-  </div>;
+class CardCollection extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentIndex: 0
+    };
+    this.handleOnClickCard = this.handleOnClickCard.bind(this);
+  }
+
+  handleOnClickCard() {}
+
+  render() {
+    const { children } = this.props;
+    const { currentIndex } = this.state;
+    return (
+      <ul className="card-collection">
+        {children.map((child, i) => {
+          const style = `card__${i}`;
+          const cardStyle =
+            currentIndex === i ? `${style} card--current` : style;
+          return (
+            <li key={i} className={cardStyle} onClick={this.handleOnClickCard}>
+              {child}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
 
 CardCollection.propTypes = {
   children: PropTypes.node
